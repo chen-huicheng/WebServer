230700a164d9f921325e1b81204b6635774de5dc
huichengchen <huicheng_chen@qq.com>
2021-05-31 20:19:22 +0800

    添加配置文件

--- include/util/config.h
+++ include/util/config.h
@@ -16,24 +16,38 @@ public:
     ~Config(){};
 
     void parse_arg(int argc, char *argv[]);
+    void parse_ini_file(string filename);
 
     //端口号
     int port;
 
     //优雅关闭链接
-    int opt_linger;
-
-    //数据库连接池数量
-    int sql_num;
+    int linger;
 
     //线程池内的线程数量
     int thread_num;
 
-    //是否关闭日志
-    int close_log;
-
     //请求队列中最大请求数量
     int max_request;
+
+    //数据库连接池数量
+    int mysql_conn_num;
+    string mysql_host;
+    string mysql_user;
+    string mysql_passwd;
+    string mysql_db_name;
+    int mysql_port;
+
+
+    //是否关闭日志
+    int close_log;
+    string log_pre_filename;
+    size_t log_buf_size;
+    size_t log_max_lines;
+
+private:
+    bool parse_line(string line,string &key,string &val);
+    void trim(string &str);
 };
 
 #endif  //WEBSERVER_CONFIG_H_
--- src/util/config.cpp
+++ src/util/config.cpp
@@ -1,15 +1,17 @@
 #include "config.h"
 #include "util.h"
+#include <fstream>
+#include <iostream>
 Config::Config()
 {
     //端口号,默认1234
     port = 1234;
 
     //优雅关闭链接，默认不使用
-    opt_linger = 0;
+    linger = 0;
 
     //数据库连接池数量,默认8
-    sql_num = 8;
+    mysql_conn_num = 8;
 
     //线程池内的线程数量,默认5
     thread_num = 5;
@@ -36,12 +38,12 @@ void Config::parse_arg(int argc, char *argv[])
         }
         case 'l':
         {
-            opt_linger = atoi(optarg);
+            linger = atoi(optarg);
             break;
         }
         case 's':
         {
-            sql_num = atoi(optarg);
+            mysql_conn_num = atoi(optarg);
             break;
         }
         case 't':
@@ -65,4 +67,129 @@ void Config::parse_arg(int argc, char *argv[])
             break;
         }
     }
+}
+void Config::parse_ini_file(string filename){
+    ifstream infile(filename.c_str());
+	if (!infile) 
+	{
+		fprintf(stderr, "file open error!");
+		return;
+	}
+    map<string,string> kv;
+    string line,key,val;
+    while (getline(infile, line)){
+        if(line.size()==0||line[0]=='#'||line[0]=='[')
+            continue;
+        if(parse_line(line,key,val))
+            kv[key]=val;
+    }
+    if(kv.find("port")!=kv.end()){
+        port=stoi(kv["port"]);
+    }
+    if(kv.find("thread_num")!=kv.end()){
+        thread_num=stoi(kv["thread_num"]);
+    }
+    if(kv.find("linger")!=kv.end()){
+        linger=stoi(kv["linger"]);
+    }
+    if(kv.find("max_request")!=kv.end()){
+        max_request=stoi(kv["max_request"]);
+    }
+    if(kv.find("mysql_host")!=kv.end()){
+        mysql_host=kv["mysql_host"];
+    }
+    if(kv.find("mysql_user")!=kv.end()){
+        mysql_user=kv["mysql_user"];
+    }
+    if(kv.find("mysql_passwd")!=kv.end()){
+        mysql_passwd=kv["mysql_passwd"];
+    }
+    if(kv.find("mysql_db_name")!=kv.end()){
+        mysql_db_name=kv["mysql_db_name"];
+    }
+    if(kv.find("mysql_port")!=kv.end()){
+        mysql_port=stoi(kv["mysql_port"]);
+    }
+    if(kv.find("mysql_conn_num")!=kv.end()){
+        mysql_conn_num=stoi(kv["mysql_conn_num"]);
+    }
+    if(kv.find("enable")!=kv.end()){
+        if(stoi(kv["enable"])==0){
+            close_log=1;
+        }
+    }
+    if(kv.find("log_pre_filename")!=kv.end()){
+        log_pre_filename=kv["log_pre_filename"];
+    }
+    if(kv.find("log_buf_size")!=kv.end()){
+        log_buf_size=stoi(kv["log_buf_size"]);
+    }
+    if(kv.find("log_max_lines")!=kv.end()){
+        log_max_lines=stoi(kv["log_max_lines"]);
+    }
+
+}
+bool Config::parse_line(string line,string &key,string &val){
+    if (line.empty())
+	{
+		return false;
+	}
+	int start_pos = 0, end_pos = line.size() - 1, pos = 0;
+	if ((pos = line.find('#')) != -1)
+	{
+		if (0 == pos)
+		{//行的第一个字符就是注释字符
+			return false;
+		}
+		end_pos = pos - 1;
+	}
+	string new_line = line.substr(start_pos, end_pos + 1 - start_pos);  // 预处理，删除注释部分
+ 
+	if ((pos = new_line.find('=')) == -1)
+	{
+		return false;  // 没有=号
+	}
+ 
+	key = new_line.substr(0, pos);
+	val = new_line.substr(pos + 1, end_pos + 1- (pos + 1));
+ 
+	trim(key);
+	if (key.empty())
+	{
+		return false;
+	}
+	trim(val);
+	return true;
+}
+void Config::trim(string &str){
+    if (str.empty())
+	{
+		return;
+	}
+	int i, start_pos, end_pos;
+	for (i = 0; i < str.size(); ++i)
+	{
+		if (!isspace(str[i]))
+		{
+			break;
+		}
+	}
+	if (i == str.size())
+	{
+		str = "";
+		return;
+	}
+ 
+	start_pos = i;
+ 
+	for (i = str.size() - 1; i >= 0; --i)
+	{
+		if (!isspace(str[i]))
+		{
+			break;
+		}
+	}
+	end_pos = i;
+ 
+	str = str.substr(start_pos, end_pos - start_pos + 1);
 }
